71a878cdbdd5b374bb1c899ac366400b7bd2ba63
Emma <emszan89@gmail.com>
2019-10-27 19:47:07 +0100

    Made Settings page

--- resources/js/components/SettingsComponent.vue
+++ resources/js/components/SettingsComponent.vue
@@ -3,15 +3,27 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Settings</div>
+                    <div class="card-header">Player settings</div>
 
                     <div class="card-body">
                         <div v-if="message.length > 0" class="alert alert-success" role="alert" v-text="message"></div>
-                        
+                        <div class="col"></div>
+                        <div class="col offset-md-4 mt-3">
+                            <label for="color">Change chat color</label>
+                            <input type="color"></div>
+                        <div class="col-md-4 offset-md-4 mt-2">
+                            <label for="changename">Change player name</label>
+                            <input class="form-control" type="text" placeholder="Player name"></div>
+                        <div class="col-md-4 offset-md-4 mt-3">
+                            <label for="changename">Change password</label>
+                            <input class="form-control" type="text" placeholder="Password"></div>
+                        <div class="col-md-4 offset-md-4 mt-3">
+                            <button type="button" class="btn btn-secondary">Save</button></div>
                     </div>
                 </div>
             </div>
         </div>
+        
     </div>
 </template>
 
@@ -26,3 +38,7 @@
         }
     }
 </script>
+
+<style>
+
+</style>
