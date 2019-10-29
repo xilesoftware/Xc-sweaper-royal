2749bd167d7fa8f1489cbf0959b0ab81a7c3cb33
Emma <emszan89@gmail.com>
2019-10-29 12:22:19 +0100

    Styled the homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>XC-SWEEPER ROYALE</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #3e3e3e;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
@@ -45,11 +45,14 @@
             }
 
             .title {
-                font-size: 84px;
+                font-size: 90px;
+                text-shadow: 3px 2px rgb(11, 128, 7);
+                font-family: "times new roman";
+                color: #96c32a;
             }
 
             .links > a {
-                color: #636b6f;
+                color: #96c32a;
                 padding: 0 25px;
                 font-size: 13px;
                 font-weight: 600;
@@ -60,6 +63,11 @@
 
             .m-b-md {
                 margin-bottom: 30px;
+            }
+
+            h1, h3 {
+                color: #96c32a; 
+                font-family: "times new roman";
             }
         </style>
     </head>
@@ -78,22 +86,14 @@
                     @endauth
                 </div>
             @endif
-
             <div class="content">
+                <h1>WELCOME TO</h1>
                 <div class="title m-b-md">
-                    Laravel
+                XC-SWEEPER ROYALE
                 </div>
+                <h3>Socialize. Battle. Have Fun!</h3>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+           
             </div>
         </div>
     </body>
